dbc24850097483e240aa2d0f95d12942357eee6e
rdrishabh <rishabh.dixit@valiancesolutions.com>
2019-07-11 15:23:23 +0530

    Stuck at present address

--- preprocessing.py
+++ preprocessing.py
@@ -21,7 +21,7 @@ def preprocess(img_grey):
     horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontalsize,1))
     horizontal = cv2.erode(horizontal, horizontalStructure, (-1, -1))
     horizontal = cv2.dilate(horizontal, horizontalStructure, (-1, -1))
-    cv2.imwrite("../temp/horizontal.jpg", horizontal)
+    cv2.imwrite("forms/horizontal.jpg", horizontal)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     #inverse the image, so that lines are black for masking
@@ -30,10 +30,10 @@ def preprocess(img_grey):
     masked_img = cv2.bitwise_and(img, img, mask=horizontal_inv)
     #reverse the image back to normal
     masked_img_inv = cv2.bitwise_not(masked_img)
-    cv2.imwrite("../temp/temp1.jpg", masked_img_inv)
+    cv2.imwrite("forms/temp1.jpg", masked_img_inv)
 
     # Vertical libe removal
-    img2 = cv2.imread("../temp/temp1.jpg")
+    img2 = cv2.imread("forms/temp1.jpg")
     img2 =cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 
     img2 = cv2.bitwise_not(img2)
@@ -43,7 +43,7 @@ def preprocess(img_grey):
     verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, verticalsize))
     vertical = cv2.erode(vertical, verticalStructure, (-1, -1), iterations=10)
     vertical = cv2.dilate(vertical, verticalStructure, (-1, -1))
-    cv2.imwrite("../vertical.jpg", vertical)
+    cv2.imwrite("forms/vertical.jpg", vertical)
 
     vertical_inv = cv2.bitwise_not(vertical)
     #perform bitwise_and to mask the lines with provided mask
@@ -51,10 +51,10 @@ def preprocess(img_grey):
     #reverse the image back to normal
     masked_img_inv2 = cv2.bitwise_not(masked_img2)
     #cv2.imshow("vertical_bitwise_not", vertical)
-    cv2.imwrite("../temp/temp2.jpg", masked_img_inv2)
+    cv2.imwrite("forms/temp2.jpg", masked_img_inv2)
 
     #Remove spots from the final output
-    image = cv2.imread('../temp/temp2.jpg')
+    image = cv2.imread('forms/temp2.jpg')
     # Denoising spots
     #image = cv2.fastNlMeansDenoisingColored(image,None,10,10,7,30)
     img_bw = 255*(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) > 5).astype('uint8')
@@ -65,10 +65,6 @@ def preprocess(img_grey):
 
     img_mask = np.dstack([img_mask, img_mask, img_mask]) / 255
     output = image * img_mask
-    cv2.imwrite('../temp/output.png', output)
+    cv2.imwrite('forms/output.png', output)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-#img = cv2.imread("./../data/input_forms/form_sample01.jpg")
-#img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#preprocess(img)
